f9efc655226abb9c15de524aa2db7b4866d44b4a
roderikmogot <roderikmogot17@gmail.com>
2022-07-27 17:07:59 +0800

    add boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { FormEvent, useState } from "react";
+
+interface Note {
+  user: string;
+  content: string;
+}
 
 const Home: NextPage = () => {
+  const [data, setData] = useState<Note>({
+    user: "",
+    content: "",
+  });
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // if succesful
+    setData({user: '', content: ''})
+  }
+
   return (
     <div>
       <Head>
@@ -12,15 +30,23 @@ const Home: NextPage = () => {
 
       <div className="w-screen h-screen p-4 flex flex-col justify-center items-center gap-8 lg:flex-row">
         <div className="w-full">
-          <h1 className="text-xl font-bold text-center">What&#39;s on your mind?</h1>
-          <form className="w-full flex flex-col justify-center items-center gap-1">
+          <h1 className="text-xl font-bold text-center">
+            What&#39;s on your mind?
+          </h1>
+          <form className="w-full flex flex-col justify-center items-center gap-1" onSubmit={e => submitHandler(e)}>
             <input
+              required
               type="text"
+              value={data.user}
+              onChange={(e) => setData({...data, user: e.target.value})}
               className="w-full border-2 border-gray-600 p-2"
               placeholder="Insert name"
             />
             <textarea
+              required
               placeholder="What are you thinking about?"
+              value={data.content}
+              onChange={(e) => setData({...data, content: e.target.value})}
               className="w-full border-2 border-gray-600 p-2"
             ></textarea>
             <button className="py-1 w-full bg-blue-400 text-white">Add</button>
@@ -32,7 +58,9 @@ const Home: NextPage = () => {
               <div className="font-bold text-lg">Thanos</div>
               <div className="text-slate-300">14/07</div>
             </div>
-            <div className="font-normal text-md">Balance as all things should be..!</div>
+            <div className="font-normal text-md">
+              Balance as all things should be..!
+            </div>
           </div>
         </div>
       </div>
